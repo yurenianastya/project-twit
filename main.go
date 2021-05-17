ae6668c17319c38b903c6b6a49af54bd814c5d10
yurenianastya <nastyayurenia@gmail.com>
2021-05-17 18:34:21 +0300

    adding grpc server w/ mongodb

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "project-twit/docs"
+	"project-twit/grpc"
+)
+
+func main() {
+	r := gin.New()
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json") // The url pointing to API definition
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+	go r.Run()
+	grpc.Server()
+}
